57ce76adc9b25cee70da10f8968b476cc5705338
elevien <-levien@math.utah.edu>
2016-10-16 14:12:31 -0600

    Edited example

--- examples/nonspatial_coupled.py
+++ examples/nonspatial_coupled.py
@@ -9,38 +9,49 @@ from pylab import *
 Nx = 1
 Np = 50
 L = 1.
-T = 10
-Nspecies = 3 #(U,V)
+T = 7
+Nspecies = 4 #(U,V)
 mesh = make_lattice1d(Nx,L)
 model = ModelHybridSplitCoupled(Nspecies,mesh)
 model.system_state[0,0] = Np
-model.system_state[1,0] = 2.
-model.system_state[3,0] = Np
-model.system_state[4,0] = 2.
+model.system_state[1,0] = Np
+model.system_state[2,0] = 2.
+model.system_state[Nspecies,0] = Np
+model.system_state[Nspecies+1,0] = Np
+model.system_state[Nspecies+2,0] = 2.
 
 
 
-r = array([1,1,0])
-p = array([2,1,0])
-model.add_reaction_fast(r,p,1.)
+r = array([0,0,1,0])
+p = array([1,0,1,0])
+model.add_reaction_fast(r,p,Np)
 
-r = array([2,0,0])
-p = array([1,0,0])
+r = array([2,0,0,0])
+p = array([1,0,0,0])
 model.add_reaction_fast(r,p,1./Np)
 
-r = array([0,1,0])
-p = array([0,0,1])
-model.add_reaction_slow(r,p,1.)
+r = array([0,2,0,0])
+p = array([1,1,0,0])
+model.add_reaction_fast(r,p,1./Np)
 
-r = array([0,0,1])
-p = array([0,1,0])
-model.add_reaction_slow(r,p,4.)
+r = array([1,0,0,1])
+p = array([1,1,0,1])
+model.add_reaction_fast(r,p,1.)
+
+
+r = array([1,0,1,0])
+p = array([1,0,0,1])
+model.add_reaction_slow(r,p,1./Np)
+
+r = array([0,1,0,1])
+p = array([0,1,1,0])
+model.add_reaction_slow(r,p,4./Np)
 
 
 
-path,clock= gillespie_hybrid(model,T,0.01)
-plt.plot(clock,path[:,0],'r+')
-plt.plot(clock,path[:,3],'k-')
+path,clock= gillespie_hybrid(model,T,0.001)
+plt.plot(clock,path[:,1],'k--')
+plt.plot(clock,path[:,Nspecies+1],'k-')
 
 
 ax = plt.gca()
